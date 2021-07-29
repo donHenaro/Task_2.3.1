151c768eda8544ffd6ab3fcebacb4e2ebc539b4d
Андрей <mgb@tut.by>
2021-07-29 15:36:00 +0300

    корректировка @RequestParam

--- 2.2.2. spring_mvc/src/main/java/web/controller/HelloController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/HelloController.java
@@ -23,15 +23,16 @@ public class HelloController {
 	}
 
 	@GetMapping(value = "/cars")
-	public String printCars(ModelMap model, @RequestParam (defaultValue = "0") Integer count) {
+	public String printCars(ModelMap model, @RequestParam (value = "count", required = false) Integer count) {
 		List<Car> cars = new ArrayList<>();
 		cars.add(new Car("Москвич", "412", 1967));
 		cars.add(new Car("ВАЗ", "2101", 1970));
 		cars.add(new Car("ЗАЗ", "968", 1975));
 		cars.add(new Car("Opel", "Astra", 1995));
 		cars.add(new Car("Peugeot", "Partner", 2002));
+
 		model.addAttribute("cars",
-				(count == 0 || count >= 5) ? cars: cars.stream().limit(count).collect(Collectors.toList()));
+				(count == null || count >= 5) ? cars: cars.stream().limit(count).collect(Collectors.toList()));
 		return "cars";
 	}
 	
