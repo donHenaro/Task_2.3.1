c94ad016cd98a1d5986045a70f89dc71a1596b1c
Андрей <mgb@tut.by>
2021-08-04 12:37:05 +0300

    кодировка

--- 2.2.2. spring_mvc/src/main/java/web/config/WebConfig.java
+++ 2.2.2. spring_mvc/src/main/java/web/config/WebConfig.java
@@ -10,6 +10,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
 
 @Configuration
 @EnableWebMvc
@@ -25,10 +27,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public SpringResourceTemplateResolver templateResolver() {
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setCharacterEncoding("UTF-8");
         templateResolver.setApplicationContext(applicationContext);
         templateResolver.setPrefix("/WEB-INF/pages/");
         templateResolver.setSuffix(".html");
-        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCacheable(true);
         return templateResolver;
     }
 
@@ -43,10 +47,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
         resolver.setCharacterEncoding("UTF-8"); //++
         resolver.setForceContentType(true); //++
         resolver.setContentType("text/html; charset=UTF-8"); //++
-        resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
     }
+
 }
